371339ee3bca8b80763b5bf6356fa31a0222f6f0
larsmostue <mostuelars@gmail.com>
2024-11-27 02:44:12 +0100

    Add project files.

---
+++ 2_4_Loekker_Oppgave-1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2_4_Loekker_Oppgave_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mySentence = "Terje er Kul";
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(mySentence);
+            }
+        }
+    }
+}
